8b3cd2647d7ee0e3fdfa8dfdb27fa0300d05be5d
sivagnani <merugagnani@gmail.com>
2022-10-21 19:00:04 +0530

    21/10/2022

--- Task-3/JS/careers.js
+++ Task-3/JS/careers.js
@@ -1,34 +1,103 @@
-function validateEmail(){
-    let e = document.getElementById("email").value;
-    const mail = /^[a-zA-Z0-9.$*]+@[a-zA-Z0-9]+.[a-zA-Z0-9]{2,}$/;
-    if(e.match(mail)){
-        document.getElementById("emailError").innerHTML="";
-        return true;
-    }
-    else{
-        document.getElementById("emailError").innerHTML="Enter valid Email";
+function require(text){
+    if(text==""){
         return false;
     }
+    else{
+        return true;
+    }
 }
-function display(){
-    let f = document.getElementById("uploadbut").value;
-    let filepath = f.split("\\");
-    let filename = filepath[filepath.length-1];
-    let d = filename.split(".")[1];
-    document.getElementById("upload").value=filename;
-    if(d=="doc" || d=="pdf" || d=="docx"){
-        document.getElementById("uploadError").innerHTML="";
-        return(true);
+function validateName(){
+    let name = document.getElementById('name').value;
+    const validname =/^[a-zA-Z\s]{4,}$/;
+    if(require(name)){
+        if(name.match(validname)){
+            document.getElementById('nameError').innerHTML="";
+            return true;
+        }
+        else{
+            document.getElementById('nameError').innerHTML="Enter valid Name";
+            return false;
+        }
     }
     else{
-        document.getElementById("uploadError").innerHTML="Enter resume in valid format";
+        document.getElementById('nameError').innerHTML="Name is required";
         return(false);
     }
 }
+function genderCheck(message) {
+    document.getElementById('genderError').innerHTML="";
+    alert("Hello "+message);
+}
+function requireGender(){
+    if(document.getElementById('male').checked || document.getElementById('female').checked){
+        document.getElementById('genderError').innerHTML="";
+        return true;
+    }
+    else{
+        document.getElementById('genderError').innerHTML="Please select Gender";
+        return false;
+    }
+}
+function validateContact(req){
+    let number = document.getElementById('contact').value;
+    const validnumber =/^[0-9]{10}$/;
+    if(number.match(validnumber) || number==""){
+        if(req==1 && number==""){
+            document.getElementById('contactError').innerHTML="Please Enter Mobile Number";
+            return false;
+        }
+        else{
+            document.getElementById('contactError').innerHTML="";
+            return true;
+        }
+    }
+    else{
+        document.getElementById('contactError').innerHTML="Enter valid Mobile Number";
+        return false;
+    }
+}
+function validateEmail(req){
+    let email = document.getElementById("email").value;
+    const mail = /^[a-zA-Z0-9.$*]+@[a-zA-Z0-9]+.[a-zA-Z0-9]{2,}$/;
+    if(email.match(mail) || email==""){
+        if(req==1 && email==""){
+            document.getElementById('emailError').innerHTML="Please Enter Email";
+            return false;
+        }
+        else{
+            document.getElementById("emailError").innerHTML="";
+            return true;
+        }
+    }
+    else{
+        document.getElementById("emailError").innerHTML = "Enter valid Email";
+        return false;
+    }
+}
+function contactbyCheck(contactby){
+    if(contactby=="reqContact"){
+        document.getElementById('reqContact').innerHTML="*";
+        document.getElementById('reqEmail').innerHTML="";
+        return validateContact(1);
+    }
+    if(contactby=="reqEmail"){
+        document.getElementById('reqEmail').innerHTML="*";
+        document.getElementById('reqContact').innerHTML="";
+        return validateEmail(1);        
+    }
+    if(contactby=="reqBoth"){
+        document.getElementById('reqContact').innerHTML="*";
+        document.getElementById('reqEmail').innerHTML="*";
+        let email = validateEmail(1);
+        let number = validateContact(1); 
+        return (email && number);
+    }
+}
 function checkForm(){
-    let e = validateEmail();
-    let d = display();
-    if(e && d){
+    let name = validateName();
+    let gender = requireGender();
+    let number = contactbyCheck();
+    if(name && gender && number){
         document.getElementById("status").style.color="green";
         document.getElementById("status").innerHTML="Success";
     }
